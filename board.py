753e8f51750715428662e80f6cb46fcd56ae7762
Kevin Kellar <kellar.kevin@gmail.com>
2017-11-17 23:39:20 +0000

    added circular and other types of transforms and testcases

--- board.py
+++ board.py
@@ -2,6 +2,8 @@
 
 class Board():
 
+	IND_CIRCLE = [0,1,2,5,8,7,6,3]
+
 	def __init__(self, string='---------'):
 		self.arr = []
 		for c in string:
@@ -12,9 +14,38 @@ class Board():
 			elif c is ' ':
 				continue
 			self.arr.append(c)
+		if len(self.arr) != 9:
+			raise AssertionError("invalid buildstring length")
 
 	def is_hard_equal(self,board):
 		for i in range(len(self.arr)):
 			if self.arr[i] != board.arr[i]:
 				return False
 		return True
+
+	def transform_circular(self, amount):
+		rboard = Board()
+		
+		for i in range(len(self.IND_CIRCLE)):
+			indx = (i+amount)%len(self.IND_CIRCLE)
+			rboard.arr[self.IND_CIRCLE[indx]] = self.arr[self.IND_CIRCLE[i]]
+
+		rboard.arr[4] = self.arr[4]
+		return rboard
+
+	def transform_clockwise(self):
+		return self.transform_circular(2)
+
+	def transform_counter_clockwise(self):
+		return self.transform_circular(-2)
+
+	def transform_180(self):
+		return self.transform_circular(4)
+
+	def print_board(self):
+		print()
+		for i in range(len(self.arr)):
+			# print(i, end=' ')
+			print(self.arr[i].upper(), end=' ')
+			if i % 3 == 2:
+				print()
--- test_menace.py
+++ test_menace.py
@@ -14,10 +14,16 @@ class testing_menace(unittest.TestCase):
 		for i in range(len(board.arr)):
 			if i // 3 is 0:
 				self.assertEqual(board.arr[i],'o')
+				self.assertNotEqual(board.arr[i],'-')
+				self.assertNotEqual(board.arr[i],'x')
 			elif i // 3 is 1:
 				self.assertEqual(board.arr[i],'-')
+				self.assertNotEqual(board.arr[i],'o')
+				self.assertNotEqual(board.arr[i],'x')
 			elif i // 3 is 2:
 				self.assertEqual(board.arr[i],'x')
+				self.assertNotEqual(board.arr[i],'o')
+				self.assertNotEqual(board.arr[i],'-')
 
 	def test_board_construction_cols1(self):
 		cstring = ''
@@ -35,7 +41,7 @@ class testing_menace(unittest.TestCase):
 			elif i % 3 is 2:
 				self.assertEqual(board.arr[i],'x')
 
-	def test_hardequal_1(self):
+	def test_board_hardequal_1(self):
 		b = Board()
 
 		cstring = ''
@@ -45,8 +51,81 @@ class testing_menace(unittest.TestCase):
 
 		board = Board(cstring)
 
+		cstring = ''
+		cstring += '-x-'
+		cstring += '--\n-'
+		cstring += '-  - -'
+
+		nboard = Board(cstring)
+
 		self.assertTrue(b.is_hard_equal(board))
 		self.assertTrue(board.is_hard_equal(b))
+		self.assertFalse(nboard.is_hard_equal(b))
+		self.assertFalse(nboard.is_hard_equal(board))
+		self.assertFalse(board.is_hard_equal(nboard))
+		self.assertFalse(b.is_hard_equal(nboard))
+
+	def test_board_tranform_clockwise1(self):
+		cstring = ''
+		cstring += 'x--'
+		cstring += '-o-'
+		cstring += 'x--'
+
+		orig = Board(cstring)
+
+		cstring = ''
+		cstring += 'x-x'
+		cstring += '-o-'
+		cstring += '---'
+
+		confirm = Board(cstring)
+
+		transout = orig.transform_clockwise()
+
+		failure = False
+
+		for i in range(len(transout.arr)):
+			if transout.arr[i] != confirm.arr[i]:
+				failure = True
+
+		if failure:
+			orig.print_board()
+			transout.print_board()
+			confirm.print_board()
+
+		for i in range(len(transout.arr)):
+			self.assertEqual(transout.arr[i],confirm.arr[i])
+
+	def test_board_tranform_clockwise2(self):
+		cstring = ''
+		cstring += 'x-x'
+		cstring += 'ox-'
+		cstring += 'x--'
+
+		orig = Board(cstring)
+
+		cstring = ''
+		cstring += 'xox'
+		cstring += '-x-'
+		cstring += '--x'
+
+		confirm = Board(cstring)
+
+		transout = orig.transform_clockwise()
+
+		failure = False
+
+		for i in range(len(transout.arr)):
+			if transout.arr[i] != confirm.arr[i]:
+				failure = True
+
+		if failure:
+			orig.print_board()
+			transout.print_board()
+			confirm.print_board()
+
+		for i in range(len(transout.arr)):
+			self.assertEqual(transout.arr[i],confirm.arr[i])
 
 
 
