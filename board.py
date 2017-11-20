f40b3dc8e95c80e5a73a62baefd107beb3df27ba
Tiger Sun <suntiger.stdt@gmail.com>
2017-11-19 22:27:53 -0800

    Added Quadtable.py and Improved Indentations

--- board.py
+++ board.py
@@ -2,62 +2,58 @@
 
 class Board():
 
-	IND_CIRCLE = [0,1,2,5,8,7,6,3]
+    IND_CIRCLE = [0, 1, 2, 5, 8, 7, 6, 3]
 
-	def __init__(self, string='---------'):
-		self.arr = []
-		for c in string:
-			if c is '\n':
-				continue
-			elif c is '':
-				continue
-			elif c is ' ':
-				continue
-			self.arr.append(c)
-		if len(self.arr) != 9:
-			raise AssertionError("invalid buildstring length")
+    def __init__(self, string='---------'):
+        self.arr = []
+        for c in string:
+            if c is '\n':
+                continue
+            elif c is '':
+                continue
+            elif c is ' ':
+                continue
+            self.arr.append(c)
+        if len(self.arr) != 9:
+            raise AssertionError("invalid buildstring length")
 
-	def is_hard_equal(self,board):
-		for i in range(len(self.arr)):
-			if self.arr[i] != board.arr[i]:
-				return False
-		return True
+    def is_hard_equal(self, board):
+        for i in range(len(self.arr)):
+            if self.arr[i] != board.arr[i]:
+                return False
+        return True
 
-	def transform_circular(self, amount):
-		rboard = Board()
-		
-		for i in range(len(self.IND_CIRCLE)):
-			indx = (i+amount)%len(self.IND_CIRCLE)
-			rboard.arr[self.IND_CIRCLE[indx]] = self.arr[self.IND_CIRCLE[i]]
+    def transform_circular(self, amount):
+        rboard = Board()
+        for i in range(len(self.IND_CIRCLE)):
+            indx = (i+amount) % len(self.IND_CIRCLE)
+            rboard.arr[self.IND_CIRCLE[indx]] = self.arr[self.IND_CIRCLE[i]]
+        rboard.arr[4] = self.arr[4]
+        return rboard
 
-		rboard.arr[4] = self.arr[4]
-		return rboard
+    def transform_clockwise(self):
+        return self.transform_circular(2)
 
-	def transform_clockwise(self):
-		return self.transform_circular(2)
+    def transform_counter_clockwise(self):
+        return self.transform_circular(-2)
 
-	def transform_counter_clockwise(self):
-		return self.transform_circular(-2)
+    def transform_180(self):
+        return self.transform_circular(4)
 
-	def transform_180(self):
-		return self.transform_circular(4)
+    def transform_flip_vertical(self):
+        rboard = Board()
+        for i in range(len(self.IND_CIRCLE)):
+            if i % 3 == 0:
+                rboard.arr[i+2] = self.arr[i]
+            elif i % 3 == 2:
+                rboard.arr[i-2] = self.arr[i]
+            else:
+                rboard.arr[i] = self.arr[i]
+        return rboard
 
-	def transform_flip_vertical(self):
-		rboard = Board()
-		
-		for i in range(len(self.IND_CIRCLE)):
-			if i % 3 == 0:
-				rboard.arr[i+2] = self.arr[i]
-			elif i % 3 == 2:
-				rboard.arr[i-2] = self.arr[i]
-			else:
-				rboard.arr[i] = self.arr[i]
-		return rboard
-
-	def print_board(self):
-		print()
-		for i in range(len(self.arr)):
-			# print(i, end=' ')
-			print(self.arr[i].upper(), end=' ')
-			if i % 3 == 2:
-				print()
+    def print_board(self):
+        print()
+        for i in range(len(self.arr)):
+            print(self.arr[i].upper(), end=' ')
+            if i % 3 == 2:
+                print()
