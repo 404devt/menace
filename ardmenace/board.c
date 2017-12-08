b11601cf9fd74f01906ee81b711347135db0a644
Kevin Kellar <kellar.kevin@gmail.com>
2017-12-08 19:30:36 +0000

    visually tested hard equal

--- ardmenace/board.c
+++ ardmenace/board.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 enum _ttt_symbol_{
 	SYMBOL_X,
 	SYMBOL_O,
 	SYMBOL_E
 };
-typedef enum _ttt_symbol_ ttt_symbol;
+typedef enum _ttt_symbol_ symb_t;
 
 struct _boardtype_ 
 {
-	ttt_symbol arr[9];
+	symb_t arr[9];
 };
-typedef struct _boardtype_ boardtype;
+typedef struct _boardtype_ board_t;
 
-char get_char_from_symbol(ttt_symbol symb)
+char get_char_from_symbol(symb_t symb)
 {
 	if (symb == SYMBOL_X)
 		return 'X';
@@ -24,7 +25,7 @@ char get_char_from_symbol(ttt_symbol symb)
 		return '-';
 }
 
-int sprint_board(char* buf, boardtype b)
+int sprint_board(char* buf, board_t b)
 {
 	int count = 0;
 	count += sprintf(buf+count,"\n");
@@ -42,23 +43,53 @@ int sprint_board(char* buf, boardtype b)
 	return count;
 }
 
-void print_board(boardtype b)
+void print_board(board_t b)
 {
 	char buf[30];
 	sprint_board(buf,b);
 	printf("%s",buf);
 }
 
+bool boards_hard_equal(board_t a, board_t b)
+{
+	for (int i = 0; i < 9; i++)
+	{
+		if (a.arr[i] != b.arr[i])
+			return false;
+	}
+	return true;
+}
+
 int main()
 {
-	boardtype board;
+	board_t a;
 	for(int i = 0; i < 9; i++)
 	{
 		if (i % 3 == 1)
-			board.arr[i] = SYMBOL_X;
+			a.arr[i] = SYMBOL_X;
 		else
-			board.arr[i] = SYMBOL_E;
+			a.arr[i] = SYMBOL_E;
 	}
-	print_board(board);
-	printf("lmao\n");
+	board_t b;
+	for(int i = 0; i < 9; i++)
+	{
+		if (i % 3 == 1)
+			b.arr[i] = SYMBOL_X;
+		else
+			b.arr[i] = SYMBOL_E;
+	}
+	board_t c;
+	for(int i = 0; i < 9; i++)
+	{
+		if (i / 3 == 2)
+			c.arr[i] = SYMBOL_X;
+		else
+			c.arr[i] = SYMBOL_E;
+	}
+	print_board(a);
+	print_board(b);
+	print_board(c);
+	printf("A = B? -> %d\n", boards_hard_equal(a,b));
+	printf("B = C? -> %d\n", boards_hard_equal(b,c));
+	printf("C = A? -> %d\n", boards_hard_equal(c,a));
 }
