f06025d89f8e6e2b18d7629c472708d76cb08807
Kevin Kellar <kellar.kevin@gmail.com>
2017-12-09 01:12:44 +0000

    partial move to a header file

--- ardmenace/board.c
+++ ardmenace/board.c
@@ -1,33 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-
-#define ID_TRANS_NONE 0
-#define ID_TRANS_CLOCKWISE 1
-#define ID_TRANS_180 2
-#define ID_TRANS_COUNTERCLOCKWISE 3
-#define ID_TRANS_FLIP_LR 4
-#define ID_TRANS_FLIP_TB 5
-
-const uint8_t IND_CIRCLE[8] = {0,1,2,5,8,7,6,3}; 
-
-
-enum _ttt_symbol_{
-	SYMBOL_X,
-	SYMBOL_O,
-	SYMBOL_E
-};
-typedef enum _ttt_symbol_ symb_t;
-
-const symb_t bbb[9] ={SYMBOL_X, SYMBOL_O, SYMBOL_E,
-			 SYMBOL_E, SYMBOL_O, SYMBOL_O,
-			 SYMBOL_X, SYMBOL_E, SYMBOL_E};
-
-struct _boardtype_ 
-{
-	symb_t arr[9];
-};
-typedef struct _boardtype_ board_t;
+#include "board.h"
 
 char get_char_from_symbol(symb_t symb)
 {
